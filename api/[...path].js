096bbdae24029157dd79ea4c3925f979e16def39
3suji3 <tnwl2799@gmail.com>
2025-12-03 11:08:33 +0900

    fix: add CORS headers to API

--- api/[...path].js
+++ api/[...path].js
@@ -5,6 +5,18 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
 const middlewares = jsonServer.defaults();
 
+// CORS 설정 추가
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 
