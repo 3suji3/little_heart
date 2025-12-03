feda608525a28083f794641c5339b05629efdcb7
3suji3 <tnwl2799@gmail.com>
2025-12-03 11:46:03 +0900

    chore: remove Vercel backend files, keep only frontend

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const Server = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' 
-        ? "https://little-heart-sujis-projects-07c6a1ca.vercel.app/api"  
-        : "http://localhost:9999"    
+    baseURL: "http://localhost:9999"    
 });
 
 export default Server;
