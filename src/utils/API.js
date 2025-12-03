3e8c13aae77b136612511f6fcf8dc3b092fae5aa
3suji3 <tnwl2799@gmail.com>
2025-12-03 10:58:03 +0900

    feat: add Vercel serverless backend

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Server = axios.create({
     baseURL: process.env.NODE_ENV === 'production' 
-        ? "YOUR_RENDER_URL_HERE"  // Render 배포 후 여기에 URL 입력
+        ? "YOUR_RENDER_URL_HERE"  
         : "http://localhost:9999"    
 });
 
